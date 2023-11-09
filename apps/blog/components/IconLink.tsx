a1c9e7db6bc05a0cd7d64260ac3ae55207ced2a0
2nonnon <lanpangzi0205@gmail.com>
2023-11-09 23:59:11 +0800

    chore: @type/react-dom

--- apps/blog/components/IconLink.tsx
+++ apps/blog/components/IconLink.tsx
@@ -1,19 +1,16 @@
 import { Icon } from '@iconify-icon/react'
-import type { Route } from 'next'
+import type { LinkProps } from 'next/link'
 import Link from 'next/link'
 import { memo } from 'react'
 
-export interface IconLinkProps {
-  title: string
-  href: Route<any> | URL
+export type IconLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps & {
   name: string
   icon: string
-  target?: string
 }
 
-export default memo(({ name, title, href, icon, target = '_self' }: IconLinkProps) => {
+export default memo(({ name, title, href, icon, locale, target = '_self' }: IconLinkProps) => {
   return (
-    <Link href={href} title={title} className="flex surface-sm hover:no-underline p-1 rounded md:px-3" target={target}>
+    <Link href={href} title={title} className="flex surface-sm hover:no-underline p-1 rounded md:px-3" locale={locale} target={target}>
       <p className='hidden md:inline-block m-0'>{name}</p>
       <Icon className='md:hidden' width={30} height={30} icon={icon} alt={name} name={name} />
     </Link>
